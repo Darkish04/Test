538c79961c436333650c5a502a75865574d4f74e
Darkish <athishhm.dev@gmail.com>
2025-04-23 17:47:45 +0530

    black ppl

---
+++ app/comp/Test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Test = () => {
+  return (
+    <div className=" bg-primary">
+      Sharan is my nigga
+    </div>
+  )
+}
+
+export default Test;
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Test from "./comp/Test";
+
+const page = () => {
+  return (
+    <div>
+      <h1>Athish</h1>
+      <Test />
+    </div>
+  );
+};
+
+export default page;
